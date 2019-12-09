f6f627a1b3e1bf79ab52d0d660bd6876a1839e36
timer <knosikay@gmail.com>
2019-12-10 03:54:59 +0800

    plot code done

--- craigslistbargain/tb2plt.py
+++ craigslistbargain/tb2plt.py
@@ -1,3 +1,9 @@
+# Example:
+# python tb2plt.py --dir apex_logs/ --agent0 a2c_decay3 a2c_decay4 --agent1 tom2 tom3 --show --max-epoch 90 \
+#       --agent1-move 50 --agent1-leftclip 50 --draw-type rl
+
+# python tb2plt.py --dir apex_logs/ --agent0 a2c_decay3 a2c_decay4 --agent1 tom2 tom3 --show --max-epoch 90
+
 
 # Load scalar from tensorboard logs and render with matplotlib
 #   * extract_rewards_from_example(file_name):
@@ -13,7 +19,7 @@ import os
 import re
 
 edge = 0.17
-left_e = 0.19
+left_e = 0.22
 up_e = 0.92
 right_e = 0.98
 
@@ -33,17 +39,25 @@ def extract_rewards_from_example(file_name='result.txt'):
 def get_args():
     parser = argparse.ArgumentParser(conflict_handler='resolve')
     parser.add_argument('--dir', default='logs/', help='directory of the tensorboard log file')
-    parser.add_argument('--draw-type', choices=['sl', 'rl', 'tom'], type=str, default='sl', help='plot type')
+    parser.add_argument('--agent0', nargs='+', type=str, help='directory of the tensorboard log file')
+    parser.add_argument('--agent1', nargs='+', type=str, help='directory of the tensorboard log file')
+    parser.add_argument('--draw-type', choices=['sl', 'rl'], type=str, default='sl', help='plot type')
     parser.add_argument('--show', action='store_true', help='show pictures directly')
-    parser.add_argument('--fig-dir', default='./', help='direcotry of these figures')
+    parser.add_argument('--fig-dirs', default='./', help='direcotry of these figures')
     parser.add_argument('--font-size', type=int, default=24, help='size of fonts')
     parser.add_argument('--label-size', type=int, default=24, help='size of font on label')
 
-    parser.add_argument('--test', type=str, nargs='+', choices=['a', 'b', 'c'], help='size of font on label')
+    # parser.add_argument('--test', type=str, nargs='+', choices=['a', 'b', 'c'], help='size of font on label')
+
+    parser.add_argument('--max-epoch', type=int, default=50, help='')
+    parser.add_argument('--plot-step', type=int, default=5, help='')
+    parser.add_argument('--train-step', type=int, default=100)
+    parser.add_argument('--one-point', default=False, action='store_true')
+    parser.add_argument('--item-name', default='agent0/reward', type=str)
+    parser.add_argument('--agent1-leftclip', default=50, type=int)
+    parser.add_argument('--agent1-move', default=0, type=int)
 
     args = parser.parse_args()
-    print(args.test)
-    quit()
     return args
 
 def load_from_tensorboard(dir):
@@ -70,28 +84,60 @@ def get_xy(it):
 
 # Supervise Learning
 # dev/train loss
-def draw_sl_training_curve(dir, args):
-    sl_scalars = load_from_tensorboard(dir)
+def draw_sl_training_curve(rl_dirs, tom_dirs, args):
+
+    plot_x = list(range(0, args.max_epoch, args.plot_step))
+    plot_y_dict = [{}, {}]
+
+    def aggregate_info(dirs, pyd, item_name='agent0/reward'):
+        for d in dirs:
+            scalars = load_from_tensorboard(d)
+            xx, yy = get_xy(scalars.Items(item_name))
+            for i, y in enumerate(yy):
+                if args.one_point and xx[i] % (args.train_step* args.plot_step) != 0:
+                    continue
+                x = (xx[i] // args.train_step - 1) // args.plot_step * args.plot_step
+                if not pyd.get(x):
+                    pyd[x] = []
+                pyd[x].append(y)
+
+    # load rl data
+    aggregate_info(rl_dirs, plot_y_dict[0], item_name=args.item_name)
+    aggregate_info(tom_dirs, plot_y_dict[1], item_name=args.item_name)
+
+    # for i in range(2):
+    #     step = 50
+    #     print('[{agent}]{step}: {mean}(+-{std})'
+    #           .format(agent=i, step=step, mean=np.mean(plot_y_dict[i][step]), std = np.std(plot_y_dict[i][step])))
+
 
     # Draw text
-    labels = ['train set', 'dev set']
+    labels = ['rl', 'tom']
     x = []
     y = []
     plt.style.use('ggplot')
 
     # Draw train set
-    x, y = get_xy(sl_scalars.Items('train/loss'))
-    plt.plot(x, y, label=labels[0])
+    for i in range(2):
+        ymean = []
+        ystd = []
+        for j, x in enumerate(plot_x):
+            yylist = plot_y_dict[i][x]
+            mean = np.mean(yylist)
+            std = np.std(yylist)
+            ymean.append(mean)
+            ystd.append(std)
+        # plt.plot(plot_x, ymean, label=labels[i])
+        plt.errorbar(plot_x, ymean, ystd, label=labels[i], alpha=0.7)
 
-    # Draw dev set
-    x, y = get_xy(sl_scalars.Items('dev/loss'))
-    plt.plot(x, y, label=labels[1])
+    plt.axhline(y=0.4, ls=":", c='gray')
+    plt.axhline(y=0, ls=":", c='gray')
 
-    plt.xlabel('Traning steps', fontsize=args.font_size)
-    plt.ylabel('Loss', fontsize=args.font_size)
-    plt.title('Suprevise Learning', fontsize=args.font_size)
+    plt.xlabel('Episode', fontsize=args.font_size)
+    plt.ylabel('Reward', fontsize=args.font_size)
+    plt.title('Sample Efficiency', fontsize=args.font_size)
     plt.tick_params(labelsize=args.label_size)
-    # plt.subplots_adjust(bottom=edge, left=left_e, top=up_e, right=right_e)
+    plt.subplots_adjust(bottom=edge, left=left_e, top=up_e, right=right_e)
     plt.legend(fontsize=args.font_size)
     if args.show:
         plt.show()
@@ -135,27 +181,57 @@ def draw_sl_training_curve(dir, args):
 #     plt.legend(fontsize=font_size)
 #     plt.show()
 
-def draw_rl_tranning_curve(dir, args):
-    rl_scalars = load_from_tensorboard(dir)
+def draw_rl_tranning_curve(rl_dirs, tom_dirs, args):
+    plot_x = [list(range(0, args.max_epoch, args.plot_step)),
+              list(range(args.agent1_leftclip, args.max_epoch, args.plot_step))]
+    plot_y_dict = [{}, {}]
+
+    def aggregate_info(dirs, pyd, item_name='agent0/reward', right_move=0):
+        for d in dirs:
+            scalars = load_from_tensorboard(d)
+            xx, yy = get_xy(scalars.Items(item_name))
+            for i, y in enumerate(yy):
+                if args.one_point and xx[i] % (args.train_step * args.plot_step) != 0:
+                    continue
+                x = (xx[i] // args.train_step - 1 + right_move) // args.plot_step * args.plot_step
+                if not pyd.get(x):
+                    pyd[x] = []
+                pyd[x].append(y)
+
+    # load rl data
+    aggregate_info(rl_dirs, plot_y_dict[0], item_name=args.item_name)
+    aggregate_info(tom_dirs, plot_y_dict[1], item_name=args.item_name, right_move=args.agent1_move)
+
+    # for i in range(2):
+    #     step = 50
+    #     print('[{agent}]{step}: {mean}(+-{std})'
+    #           .format(agent=i, step=step, mean=np.mean(plot_y_dict[i][step]), std = np.std(plot_y_dict[i][step])))
 
     # Draw text
-    labels = ['RL Agent', 'SL Agent']
+    labels = ['rl', 'tom']
     x = []
     y = []
     plt.style.use('ggplot')
-    plt.figure(figsize=(6.3, 5))
 
     # Draw train set
-    x, y = get_xy(rl_scalars.Items('agent0/reward'))
-    plt.plot(x, y, label=labels[0])
+    for i in range(2):
+        ymean = []
+        ystd = []
+        for j, x in enumerate(plot_x[i]):
+            yylist = plot_y_dict[i][x]
+            mean = np.mean(yylist)
+            std = np.std(yylist)
+            ymean.append(mean)
+            ystd.append(std)
+        # plt.plot(plot_x, ymean, label=labels[i])
+        plt.errorbar(plot_x[i], ymean, ystd, label=labels[i], alpha=0.7)
 
-    # Draw dev set
-    x, y = get_xy(rl_scalars.Items('agent1/reward'))
-    plt.plot(x, y, label=labels[1])
+    plt.axhline(y=0.4, ls=":", c='gray')
+    plt.axhline(y=0, ls=":", c='gray')
 
-    plt.xlabel('Traning steps', fontsize=args.font_size)
+    plt.xlabel('Episode', fontsize=args.font_size)
     plt.ylabel('Reward', fontsize=args.font_size)
-    plt.title('Reinforcement Learning', fontsize=args.font_size)
+    plt.title('Inference Fine Tuning', fontsize=args.font_size)
     plt.tick_params(labelsize=args.label_size)
     plt.subplots_adjust(bottom=edge, left=left_e, top=up_e, right=right_e)
     plt.legend(fontsize=args.font_size)
@@ -168,12 +244,15 @@ if __name__ == '__main__':
     args = get_args()
     # data = load_from_tensorboard(args.dir)
 
+    dirs0 = [args.dir+i for i in args.agent0]
+    dirs1 = [args.dir+i for i in args.agent1]
+
     if args.draw_type == 'sl':
         # Draw sl curve
-        draw_sl_training_curve(args.dir, args)
+        draw_sl_training_curve(dirs0, dirs1, args)
     elif args.draw_type == 'rl':
         # Draw rl curve
-        draw_rl_tranning_curve(args.dir, args)
+        draw_rl_tranning_curve(dirs0, dirs1, args)
     elif args.draw_type == 'tom':
         pass
 
